aed3c5e6bb0cb9d4f58332eba797ed626e85eb43
FDC.Nars <fdc.narsing@gmail.com>
2022-04-13 10:03:41 +0800

    add condition

--- fibonacci.php
+++ fibonacci.php
@@ -9,15 +9,22 @@
         
         function calcFibo () {
             document.getElementById("output").innerHTML = "";
-            var x = 0,  y = 1, z, i;  
+            var x = 0, y = 1, z, i;  
+            var errorMessage = "more than 0 badi para naay result";
+
             let inputNum = document.getElementById('limit').value;
             // do fibonacci
-            for ( i = 1; i <= inputNum; i++ ){  
-                document.getElementById("output").innerHTML += x + "<br>"; 
-                z = x + y; 
-                x = y;
-                y = z; 
-            }  
+            if(inputNum.includes('-')||inputNum.includes('0')){
+                alert(errorMessage); 
+            }
+            else{
+                for ( i = 1; i <= inputNum; i++ ){  
+                    document.getElementById("output").innerHTML += x + "<br>"; 
+                    z = x + y; 
+                    x = y;
+                    y = z; 
+                }  
+            }
         }
 
         function checkQuarantine () {
@@ -25,9 +32,15 @@
             var tooOld = "you are too old. stay inside!";
             var tooYoung = "you are too young. stay inside!";
             var goBantayan = "go to bantayan";
+            var errorMessage = "di pwede negative badi";
+        
             let inputAge = document.getElementById('age').value;
             // 21 -> lower
             // ouptut -> you are too young. stay inside!
+            if(!inputAge.indexOf('-')){
+                alert(errorMessage); 
+                return false;
+            }
             if ( inputAge < 21 || inputAge > 65 ) {
                 if (inputAge > 65){
                     verdict = tooOld;
@@ -45,11 +58,11 @@
     </script>
 </head>
 <body>
-    <input type="text" id="limit">
+    <input type="number" id="limit" min="1">
     <button onclick="calcFibo()">calculate range</button>
     <div id="output"></div>
 
-    <input type="text" id="age">
+    <input type="number" id="age">
     <button onclick="checkQuarantine()">calculate range</button>
     <div id="output_age"></div>
 </body>
